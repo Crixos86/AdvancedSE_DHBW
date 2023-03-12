f1753a0c6f275adb26909e1b34714f0cbdcf98a1
Crixos86 <dome.veith86@gmail.com>
2023-03-12 11:51:57 +0100

    Simpsons Characters Picture added

--- SimpsonsGame/src/main/java/de/dhbw/ase/simpsons/application/CharacterAction.java
+++ SimpsonsGame/src/main/java/de/dhbw/ase/simpsons/application/CharacterAction.java
@@ -3,6 +3,8 @@ package de.dhbw.ase.simpsons.application;
 public interface CharacterAction {
     void introduce();
 
+    void printPicture();
+
     void presentHomeAndWorkplace();
     void giveQuote();
 }
